7cf5b06fcc760a756c924b2271eda77db2ca7250
Malte <mf@creavio.de>
2020-12-12 14:19:07 +0100

    Test React

--- src/domain/widget/widget.tsx
+++ src/domain/widget/widget.tsx
@@ -3,16 +3,18 @@ import React, {useEffect, useState} from 'react';
 export default function Widget() {
 
   const [time, setTime] = useState(new Date());
+  const [random, setRandom] = useState(0);
 
   useEffect(() => {
     setInterval(() => {
       setTime(new Date());
+      setRandom(Math.random());
     }, 1000);
   }, []);
 
   return (
     <div>
-      Hello from React: {time.getSeconds()}
+      Hello from React: {time.getSeconds()} {random}
     </div>
   );
 }
