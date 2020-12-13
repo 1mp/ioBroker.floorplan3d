a249a601df19f911fcf5ecb4cf0d41a723c0abbe
Malte <mf@creavio.de>
2020-12-13 13:53:31 +0100

    Test React vis Object

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Widget from './domain/widget/widget';
 
 export interface AppProps {
-  data: any;
+  data: { oid: string | undefined };
 }
 
 export function App({ data }: AppProps) {
   console.log('data', data);
 
+  console.log('vis type', typeof vis);
+  const [watch] = useState(data.oid);
+
+  // vis.states.bind(data.oid_command + '.val', onChange);
+
   return (
-    <Widget/>
+    <>
+      <Widget/>
+      <br/>
+      Watch: {watch}
+    </>
   );
 }
